6311c5cfc450d3b3defbdae4d326de298b9f4296
won-matthew-goh <qimo2000@naver.com>
2021-08-04 15:11:16 +0900

    5교시 (사진 레디우스 적용안됨)

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+/* 
+false 판정: 0, null, undefined, '', false
+- 0을 제외한 모든 숫자
+- ''(빈문자열)을 제외한 모든 문자
+- 객체, 함수 ... 전부 다
+true 판정: false 판정 외 모든 것.
+*/
+
 /* *************** global init **************** */
 // console.log(firebase);
 var auth = firebase.auth();
@@ -9,15 +17,32 @@ var googleAuth = new firebase.auth.GoogleAuthProvider();
 
 
 /* *************** event callback **************** */
-function onAuthChanged(user) {
+function onAuthChanged(user) { // auth 상태가 변하면 알려줘
   console.log(user);
+  if(user) {
+    $('.bt-login').hide();
+    $('.bt-logout').show();
+    $('.photo-logo img').attr('src', user.photoURL);
+    $('.photo-logo').show();
+    $('.icon-logo').hide();
+  }
+  else {
+    $('.bt-login').show();
+    $('.bt-logout').hide();
+    $('.photo-logo').hide();
+    $('.icon-logo').show();
+  }
 };
 
 function onLogin() {
   auth.signInWithPopup(googleAuth);
 };
 
-/* *************** event init **************** */
-auth.onAuthStateChanged(onAuthChanged);
-$('.bt-login').click(onLogin);
+function onLogout() {
+  auth.signOut();
+};
 
+/* *************** event init **************** */
+auth.onAuthStateChanged(onAuthChanged); // auth 상태가 변하면 알려줘
+$('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
