da0074bc7fea3ec3004814c6495e3a23481cc0e4
won-matthew-goh <qimo2000@naver.com>
2021-08-19 16:05:50 +0900

    footer 위치잡기

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,13 @@
 
 
 /*************** event callback *************/
-
+function onResize() {
+  var minHeight = $(this).outerHeight() - $('.header-wrapper').outerHeight(true) - $('.footer-wrapper').outerHeight(true) - $('.recent-wrapper').outerHeight(true) - 24;
+  $('.list-wrapper').css('min-height', minHeight+'px');
+}
 
 /*************** event init *****************/
-
+$(window).resize(onResize).trigger('resize');
 
 /*************** start init *****************/
 
